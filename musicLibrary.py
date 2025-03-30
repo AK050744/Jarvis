83a5aed9893eab42a543ec3e39a2760e6325023a
AK050744 <anshvikas04@gmail.com>
2025-03-31 01:10:44 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "skyfall": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "football":"https://www.youtube.com/watch?v=pRpeEdMmmQ0",
+    "lotus": "https://www.youtube.com/watch?v=gB03rJGwxJA&list=OLAK5uy_nVtYb2ra540JPv_kwpeoj2GJrtJZmWgyE",
+    "japan": "https://www.youtube.com/watch?v=qal34e9v_pk",
+    "smile": "https://www.youtube.com/watch?v=kPa7bsKwL-c"
+}
